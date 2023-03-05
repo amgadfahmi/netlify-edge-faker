3393164133641f58f876786f5e1fba5d6e7cf98b
Amgad Fahmi <amgadnady@gmail.com>
2023-03-05 13:43:18 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+## Netlify Edge Function + Faker JS 
+
+The function expecting a post request with a category and final api function name and its parameters, all the APIs references can be found here:
+
+**FakerJS API**
+https://fakerjs.dev/api/
+
+
+##Data In-Out
+A sample post request body should look like this 
+
+``` json 
+{
+    "name": {
+        "firstName": "",
+        "lastName": "female"
+    },
+    "animal": {
+        "bear": ""
+    },
+    "lorem": {
+        "word": 8
+    }
+}
+```
+And response : 
+```json 
+[
+    {
+        "firstName": "Mossie",
+        "lastName": "Cremin",
+        "bear": "Spectacled bear",
+        "word": "possimus"
+    }
+]
+```
+
+##Query parameters 
+The function can accept only one parameter which is **repeat**, the value passed for this parameter will affect the number of results returning, check the below example 
+
+http://function-url/fake-me?repeat=2
+
+Applying this to the first request we sent, we would be expecting a result like this: 
+
+```json
+[
+    {
+        "firstName": "Lorena",
+        "lastName": "Lubowitz",
+        "bear": "American black bear",
+        "word": "repellat"
+    },
+    {
+        "firstName": "Alva",
+        "lastName": "Leffler",
+        "bear": "American black bear",
+        "word": "voluptas"
+    }
+]
+```
---
+++ netlify/edge-functions/fake-me.ts
@@ -0,0 +1,55 @@
+import * as faker from 'https://cdn.skypack.dev/@faker-js/faker@v7.4.0?dts';
+import lodash from 'https://cdn.skypack.dev/lodash';
+import type { Context } from 'https://edge.netlify.com';
+
+export default async (
+  request: Request,
+  context: Context
+): Promise<Response> => {
+  if (request.method !== 'POST')
+    return await new Response(
+      JSON.stringify({ error: 'Only POST method allowed.' }),
+      {
+        status: 400,
+        headers: {
+          'content-type': 'application/json; charset=utf-8',
+        },
+      }
+    );
+  let repeat = 1;
+
+  const url = new URL(request.url);
+  if (url.searchParams.has('repeat')) {
+    const repeatParam = url.searchParams.get('repeat') ?? '';
+    repeat = !isNaN(+repeatParam) ? Number(repeatParam) : 1;
+  }
+
+  const result: Record<string, string>[] = [];
+
+  if (request.body) {
+    const body = await request.json();
+    for (let i = 0; i < repeat; i++) {
+      result.push(createRecord(body));
+    }
+  }
+
+  return await new Response(JSON.stringify(result), {
+    status: 200,
+    headers: {
+      'content-type': 'application/json; charset=utf-8',
+    },
+  });
+};
+
+function createRecord(body: any) {
+  const result: Record<string, string> = {};
+
+  for (const parent in body) {
+    const namespace = parent.toString();
+    for (const child in body[parent]) {
+      const param = body[parent][child];
+      result[child] = faker.faker[namespace][child](param);
+    }
+  }
+  return result;
+}
